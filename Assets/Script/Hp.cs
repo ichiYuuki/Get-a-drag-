2e8c1d88464f40a7321391bd2406f2eebe4b193e
nemotoyoshiyuki <x0703x@gmail.com>
2016-03-30 21:17:56 +0900

    Hpバー、エクスプロージョンの処理

---
+++ Assets/Script/Hp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Hp : MonoBehaviour {
+    private Slider slider;
+    private float hp = 100; //初期体力
+
+    void Start () {
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    void Update () {
+        //hp -= 1f;
+        slider.value = hp;
+	}
+}

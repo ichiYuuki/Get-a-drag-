27df064952c2d16ef46a17210db4899cdb5b7961
ichiYuuki <mrofushi@gmail.com>
2016-03-31 17:44:36 +0900

    クリアタイム計測

--- Assets/Script/GameResult.cs
+++ Assets/Script/GameResult.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System;
 public class GameResult : Stopwatch {
 	private float highScre;
-	private int highScre00;
+	private float highScre00;
 	//	List<int> highScre = new List<int> ();
 	
 	public Text clearTime;
@@ -13,24 +13,25 @@ public class GameResult : Stopwatch {
 	public GameObject parts;
 	
 	void Start () {
-//		if (PlayerPrefs.HasKey ("HighScre_a")) {
-//			highScre = PlayerPrefs.GetFloat ("HighScre_a");
-//		} 
-//		//		if (PlayerPrefs.HasKey ("HighScre00")) {
-//		//			highScre00 = PlayerPrefs.GetInt ("HighScre00");
-//		//		}
-//		else {
-//			//			double a = 59;
-//			//			double b = 59;
-//			//			double c = 99;
-//			highScre = 4000;
-//			highScre00 = 999;
-//		}
-//		//		highScre = "59:59.99";
-//		//		Debug.Log(highScre + "ハイスコア");
+		if(Application.loadedLevelName == "test_ichi"){
+			if (PlayerPrefs.HasKey ("HighScre_a")) {
+				highScre = PlayerPrefs.GetFloat ("HighScre_a");
+			} else {
+				highScre = 1000;
+			} 
+		}
+
+		if(Application.loadedLevelName == "test_ichi1"){
+			if (PlayerPrefs.HasKey ("HighScre_b")) {
+				highScre00 = PlayerPrefs.GetFloat ("HighScre_b");
+			} else {
+				highScre00 = 1000;
+			} 
+		}
+		Debug.Log (highScre + " StarthighScre");
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
 		if (ClearZone.gameClear == true) {
 			parts.SetActive(true);
@@ -38,37 +39,46 @@ public class GameResult : Stopwatch {
 			float Sec = Mathf.FloorToInt(Stopwatch.SecCount);
 			float DecCount = Mathf.FloorToInt(Stopwatch.DecCount);
 			float result = Min * 60 + Sec + DecCount /100;
-			clearTime.text = "ClearTime   " + Min + ":" + Sec + "." + DecCount;
+			String Secs = String.Format ("{0:D2}", (int)Sec);
+			String DecCounts = String.Format("{0:D2}",(int)DecCount);
+			clearTime.text = "ClearTime:  " + Min + ":" + Secs + "." + DecCounts;
 
-//			if(Application.loadedLevelName == "test_ichi"){
-//				bestTime.text = "BestTime:   " + keisan(highScre);//keisan(highs)
-//			}
-//			if(Application.loadedLevelName == "test_ichi"){
-//				if(highScre > result){
-//					PlayerPrefs.SetFloat("HighScre_a",result);
-//				}
-//				
-//			}
-			Debug.Log(keisan(highScre) + "keisan");
+			if(Application.loadedLevelName == "test_ichi"){
+				bestTime.text = "BestTime:   " + keisan(highScre);//keisan(highs)
+			}
+			if(Application.loadedLevelName == "test_ichi"){
+				if(highScre > result){
+					PlayerPrefs.SetFloat("HighScre_a",result);
+				}
+			}
+			Debug.Log(Secs + "Secs");
 			Debug.Log(clearTime.text);
-			
-			//			if(Application.loadedLevelName == "TestStage"){
-			//				bestTime.text = "BestTime:" + highScre00;
-			//			}
-			//			if(Application.loadedLevelName == "TestStage"){
-			//				if(highScre00 > result){
-			//					PlayerPrefs.SetInt("HighScre00", result);
-			//				}
-			//			}
+			Debug.Log(highScre + "highScre");
+
+			if(Application.loadedLevelName == "test_ichi1"){
+				bestTime.text = "BestTime:" + highScre00;
+			}
+			if(Application.loadedLevelName == "test_ichi1"){
+				if(highScre00 > result){
+					PlayerPrefs.SetFloat("HighScre00", result);
+				}
+			}
 		}
-		//		Debug.Log(Application.loadedLevelName);
-		//		Debug.Log (PlayerPrefs.GetInt ("HighScre00"));
+//				PlayerPrefs.DeleteAll();//データ初期化
+//				Debug.Log(Application.loadedLevelName);
+//				Debug.Log (PlayerPrefs.GetInt ("HighScre00"));
 	}
 	String keisan(float aaa){
-		double g = aaa % 100;
-		double k = aaa;
-		double h = k / 60;
-		return (int)h + ":" + (int)k + "." + (int)g;
+		double h = aaa / 60;
+		double k = aaa % 60;
+		double g = (aaa % 1 ) * 100;
+//		String H = String.Format ("{0:D2}", (int)h);
+		String K = String.Format ("{0:D2}", (int)k);
+		String G = String.Format ("{0:D2}", (int)g);
+		Debug.Log(String.Format("{0:D2}",(int)k) + " debug K");
+//		Debug.Log(g);
+		return (int)h + ":" + K + "." + G;
+
 	}
 	
 	//	public void OnRetry(){

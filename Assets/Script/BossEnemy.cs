dfb3a7e5687cc0dbef312cdeb6128a59db456319
den0722 <the.last.fortress@hotmail.co.jp>
2016-03-31 21:03:56 +0900

    boss挙動

--- Assets/Script/BossEnemy.cs
+++ Assets/Script/BossEnemy.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class BossEnemy : MonoBehaviour {
 
+	public int hp = 100;
+	public int attackPoint = 2; 
 	private bool battleStart = false;
 	public float shotDelay = 2f;
 	public GameObject bullet;
 	public GameObject[] shotPoint;
 	public bool canMove = false;
 	public int num = 0;
+	float time = 0;
+	Vector3 pos;
 
 	// Use this for initialization
 	IEnumerator Start () {
+		pos = transform.position;
 
 //		while(!battleStart){
 //
@@ -29,16 +34,22 @@ public class BossEnemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		if (canMove) {
+			Move (0);
+		}
 	}
 
 	void shot(GameObject obj, Transform tf){
+		bullet.GetComponent<EnemyBullet> ().power = attackPoint;
 		Instantiate (obj, tf.position, tf.rotation);
 	}
 
 	void Move(int num){
+		time += Time.deltaTime;
+
 		switch(num){
 		case 0:
+			transform.position = new Vector3(pos.x, pos.y + Mathf.Sin(90 * time * Mathf.Deg2Rad), pos.z);
 			break;
 		}
 	}

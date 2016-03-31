f42bba771fd552e437a03e58f7d0465feac1b176
den0722 <the.last.fortress@hotmail.co.jp>
2016-03-31 20:30:58 +0900

    ボスの挙動っぽいものを作成しました

---
+++ Assets/Script/BossEnemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossEnemy : MonoBehaviour {
+
+	private bool battleStart = false;
+	public float shotDelay = 2f;
+	public GameObject bullet;
+	public GameObject[] shotPoint;
+	public bool canMove = false;
+	public int num = 0;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+//		while(!battleStart){
+//
+//			yield return new WaitForEndOfFrame();
+//		}
+
+		while(true){
+			for(int i = 0; i < shotPoint.Length; i++){
+				shot (bullet, shotPoint[i].transform);
+			}
+
+			yield return new WaitForSeconds(shotDelay);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void shot(GameObject obj, Transform tf){
+		Instantiate (obj, tf.position, tf.rotation);
+	}
+
+	void Move(int num){
+		switch(num){
+		case 0:
+			break;
+		}
+	}
+}

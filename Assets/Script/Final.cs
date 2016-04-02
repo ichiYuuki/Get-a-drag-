b7356a62cd3187c438d90ddd1e63cca6d144a71f
ichiYuuki <mrofushi@gmail.com>
2016-04-02 15:57:50 +0900

    4/2

--- Assets/Script/Final.cs
+++ Assets/Script/Final.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Final : MonoBehaviour {
 
 	IEnumerator Start () {
-		yield return new WaitForSeconds(5);
-		Application.LoadLevel("Title");
+		if(Application.loadedLevelName == "Ending") {
+			yield return new WaitForSeconds(5);
+			Application.LoadLevel("MainTitle");
+		}
+
+	}
+
+	void Update(){
+
 	}
 }

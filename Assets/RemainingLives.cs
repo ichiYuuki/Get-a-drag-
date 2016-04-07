a54697c793d870455d3a647954bb9e154326792b
nemotoyoshiyuki <x0703x@gmail.com>
2016-04-07 21:07:35 +0900

    残機途中

---
+++ Assets/RemainingLives.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemainingLives : MonoBehaviour
+{
+    private GameObject lives;
+    private GameObject life1;
+    private GameObject life2;
+    private int lifeCounter = 3;
+    public GameObject RevivePoint;
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+         lives = GameObject.Find("Lives");
+         life1 = GameObject.Find("life1");
+        life2 = GameObject.Find("life2");       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LifeCounterDown()
+    {
+        lifeCounter--;
+        Debug.Log(lifeCounter);
+        RespawnCheck();
+    }
+
+    public void RespawnCheck()
+    {
+        if (lifeCounter == 2)
+        {                    
+            Destroy(life2);          
+        }
+        else if (lifeCounter == 1)
+        {
+            Destroy(life1);          
+        }
+        else if (lifeCounter == 0)
+        {
+            Debug.Log("GameOver");
+        }
+        Respawn();
+    }
+
+    public void Respawn()
+    {    
+        //復活させる        
+        var Clone = Instantiate(player, new Vector3(0,5,0), Quaternion.identity);
+        Clone.name = player.name;
+        Debug.Log("復活");        
+    }
+}

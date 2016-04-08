8d6754ca8f307dc3aa57dda46877bd055b44a051
ichiYuuki <mrofushi@gmail.com>
2016-04-08 19:30:39 +0900

    クリア後の挙動変更

--- Assets/Script/BGMSounds.cs
+++ Assets/Script/BGMSounds.cs
@@ -2,26 +2,47 @@
 using System.Collections;
 
 public class BGMSounds : MonoBehaviour {
-	private AudioSource[] bgmScurce;
+//	public AudioClip[] bgmClip;
+//	private AudioSource[] bgmScurce;
+
+	public AudioClip bgmClip1;
+	public AudioClip bgmClip2;
+	public AudioClip bgmClip3;
+
+	private AudioSource audioScurce1;
+	private AudioSource audioScurce2;
+	private AudioSource audioScurce3;
 
 	void Start () {
-		bgmScurce = gameObject.GetComponents<AudioSource>();
+//		bgmScurce = gameObject.GetComponents<AudioSource>();
+		audioScurce1 = gameObject.GetComponent<AudioSource>();
+		audioScurce2 = gameObject.GetComponent<AudioSource>();
+		audioScurce3 = gameObject.GetComponent<AudioSource>();
+
+		audioScurce1.clip = bgmClip1; 
+		audioScurce2.clip = bgmClip2; 
+		audioScurce3.clip = bgmClip3; 
+
+		if(Application.loadedLevelName == "MainStage1"){
+			Debug.Log("ステージ1");
+			audioScurce1.Play();
+		}else if(Application.loadedLevelName == "MainStage2"){
+//			bgmScurce[0].Stop();
+			if(BossStage.bossArea){
+				Debug.Log("ステージ2" + BossStage.bossArea);
+//				bgmScurce[1].Stop();
+				audioScurce2.Play();
+			}else{
+				Debug.Log("ステージ2Boss");
+//				bgmScurce[2].Play();
+				audioScurce3.Play();
+			}
+		}
 	}
 
 	void Update () {
-		bgmScurce[0].Play();
-//		if(Application.loadedLevelName == "MainStage1"){
-//			Debug.Log("ステージ1" + bgmScurce[0].name);
-//			bgmScurce[0].Play();
-//		}else if(Application.loadedLevelName == "MainStage2"){
-//			bgmScurce[0].Stop();
-//			if(BossStage.bossArea){
-//				Debug.Log("ステージ2" + BossStage.bossArea);
-//				bgmScurce[1].Stop();
-//			}else{
-//				Debug.Log("ステージ2Boss");
-//				bgmScurce[2].Play();
-//			}
-//		}
+
+
+
 	}
 }

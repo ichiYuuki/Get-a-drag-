d3522f177ced784cd0e74b45af6bcea02d88420c
ichiYuuki <mrofushi@gmail.com>
2016-04-11 19:11:15 +0900

    aaa

---
+++ Assets/VanishBlock.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VanishBlock : MonoBehaviour {
+	public bool block;
+	public GameObject vblock;
+	public GameObject parts;
+
+	void Start () {
+		vblock = GameObject.Find ("VBrock");
+		parts.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Invoke ("CallFinal", 4);
+	}
+	void Vanish(){
+
+	}
+}

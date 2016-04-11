fb87c76876b582cc6779a4a8c4f9a91215267ba8
den0722 <the.last.fortress@hotmail.co.jp>
2016-04-11 20:11:15 +0900

    mobile対応関連

---
+++ Assets/SetButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetButton : MonoBehaviour {
+
+	private GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Buyer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!player){
+			player = GameObject.FindWithTag("Buyer");
+		}
+	}
+
+	public void OnClick(int num){
+		player.GetComponent<Player> ().OnClick (num);
+	}
+}
